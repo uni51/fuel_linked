34e15db0e81ebf420264859a9e65536f5609ddc2
uni51 <uni51@users.noreply.github.com>
2020-02-01 13:47:00 +0900

    Chapter4. ビューへの値の受け渡し

---
+++ fuel/app/views/sample/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <title>ビューへの値の受け渡し</title>
+</head>
+<body>
+<div id="wrap">
+    <div id ="head">
+        <h1>ビューへの値の受け渡し</h1>
+    </div>
+
+    <div id="content">
+        <p>あなたの名前は「<?php print($name); ?>」ですね。</p>
+    </div>
+
+    <div id="foot">
+        <p>&copy; OMOKAJI</p>
+    </div>
+</div>
+</body>
+</html>
