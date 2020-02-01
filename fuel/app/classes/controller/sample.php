6f121f3384ba96d07c34de7d0708ef9709df5b73
uni51 <uni51@users.noreply.github.com>
2020-02-01 22:10:12 +0900

    Chapter4. テンプレートを利用する（コメント追加）

--- fuel/app/classes/controller/sample.php
+++ fuel/app/classes/controller/sample.php
@@ -2,6 +2,9 @@
 
 class Controller_Sample extends Controller_Template {
 
+    /**
+     * Chapter4-5：テンプレートを利用する
+     */
     public function action_index() {
         $this->template->title = 'お問い合わせ';
         $data = array();
@@ -9,11 +12,17 @@ class Controller_Sample extends Controller_Template {
         $this->template->content = View::forge('sample/index', $data);
     }
 
+    /**
+     * Chapter4-5：テンプレートを利用する
+     */
     public function action_check() {
         $this->template->title = 'お問い合わせ：確認';
         $this->template->content = View::forge('sample/check');
     }
 
+    /**
+     * Chapter4-5：テンプレートを利用する
+     */
     public function action_thanks() {
         $this->template->title = 'お問い合わせ：完了';
         $this->template->content = View::forge('sample/thanks');
