2b5d342bb86e6d356652eaa7c7c9eb50f7e4be7c
uni51 <uni51@users.noreply.github.com>
2020-02-01 22:47:10 +0900

    Chapter4. Model_Crudクラスを利用する2

--- fuel/app/classes/controller/shop.php
+++ fuel/app/classes/controller/shop.php
@@ -3,10 +3,30 @@
 class Controller_Shop extends Controller {
 
     public function action_index() {
-        $data['rows'] = Model_Item::find_all();
-        // $data['rows'] = Model_Item::find_all(1, 1);
+        // $data['rows'] = Model_Item::find_all();
+        // $data['rows'] = Model_Item::find_all(1, 1); // limit, offset
         // $data['row'] = Model_Item::find_by_pk(3);
+
+        // $data['rows'] = Model_Item::find_by('price', '100', '<=' );
+         $data['row'] = Model_Item::find_one_by('item_name', 'バナナ', '=' );
 
         return View::forge('shop/index', $data);
     }
+
+    public function action_save() {
+        $item = Model_Item::forge();
+
+        $data = array();
+        $data['item_name'] = '柿';
+        $data['price'] = 90;
+
+        $item->set($data);
+        $item->save();
+    }
+
+    public function action_delete() {
+        $item = Model_Item::find_by_pk(9);
+        $item->delete();
+    }
+
 }
