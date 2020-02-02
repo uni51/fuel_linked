159bfb37beaee35904efb6df0288a808df884407
uni51 <uni51@users.noreply.github.com>
2020-02-02 12:35:09 +0900

    Chapter5.Sessionに情報を記録する

--- fuel/app/views/contact/index.php
+++ fuel/app/views/contact/index.php
@@ -21,7 +21,12 @@
         </tr>
         <tr>
             <th><?php print(Form::label('性別', 'gender')); ?></th>
-            <td><?php print(Form::radio('gender', '男性', array('id'=>'form_gender_male'))); ?> <?php print(Form::label('男性', 'gender_male')); ?>　<?php print(Form::radio('gender', '女性', array('id'=>'form_gender_female', 'checked'=>'checked'))); ?> <?php print(Form::label('女性', 'gender_female')); ?></td>
+            <td>
+                <?php print(Form::radio('gender', '男性', array('id'=>'form_gender_male'))); ?>
+                <?php print(Form::label('男性', 'gender_male')); ?>　
+                <?php print(Form::radio('gender', '女性', array('id'=>'form_gender_female', 'checked'=>'checked'))); ?>
+                <?php print(Form::label('女性', 'gender_female')); ?>
+            </td>
         </tr>
         <tr>
             <th></th>
